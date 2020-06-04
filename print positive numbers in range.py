bca056ae0624c6922fdc98e33b18a8c10c10d6be
praneethk2000 <66408609+praneethk2000@users.noreply.github.com>
2020-06-04 13:31:47 +0530

    Add files via upload
    This program prints out positive integers from a list

---
+++ print positive numbers in range.py
@@ -0,0 +1,13 @@
+# Print positive integers from list
+
+list1 = [12, -7, 5, 64, -14]
+print list1
+for i in list1:
+    if i > 0:
+        print i
+
+list2 = [12, 14, -95, 3]
+print list2
+for i in list2:
+    if i > 0:
+        print i
