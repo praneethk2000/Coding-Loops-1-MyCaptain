3a52b5c29255d0a293cc34817f472e97b2794a39
praneethk2000 <66408609+praneethk2000@users.noreply.github.com>
2020-06-05 16:16:44 +0530

    Update print positive numbers in range.py

--- print positive numbers in range.py
+++ print positive numbers in range.py
@@ -2,12 +2,20 @@
 
 list1 = [12, -7, 5, 64, -14]
 print list1
+positive_list1 = []
+
 for i in list1:
     if i > 0:
-        print i
+        positive_list1.append(i)
+
+print positive_list1
 
 list2 = [12, 14, -95, 3]
 print list2
-for i in list2:
-    if i > 0:
-        print i
+positive_list2 = []
+
+for j in list2:
+    if j > 0:
+        positive_list2.append(j)
+
+print positive_list2
